1605b0289bf0bea349025c61891cf7d4853d6f1f
roline <roline@192.168.0.119>
2015-05-22 19:14:16 +0300

    Create new class

---
+++ office/src/main/java/com/office/logos/model/Message.java
@@ -0,0 +1,46 @@
+package com.office.logos.model;
+
+public class Message {
+
+	int a;
+
+	public Message(int a) {
+		this.a = a;
+	}
+
+	public int getA() {
+		return a;
+	}
+
+	public void setA(int a) {
+		this.a = a;
+	}
+
+	@Override
+	public String toString() {
+		return "Message [a=" + a + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + a;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Message other = (Message) obj;
+		if (a != other.a)
+			return false;
+		return true;
+	}
+
+}
